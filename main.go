467ad2863c9d88f8fc80c21d9c34bc3358ccd882
Samir Mishra <smir.mishra1551@gmail.com>
2021-04-26 20:25:40 +0545

    random commet

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
-	// ("html/template")
+	("html/template")
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
@@ -15,20 +15,23 @@ func main() {
 	http.ListenAndServe(":8080", nil)
 }
 
-// func html(){
-// 	<h1>editing {{title}}</h1>
-// 	<form action="/save/{{.Title}}" method="POST">
-// 	<div><textarea name="body" rows="20" cols="80">{{printf "%s" .Body}}</textarea></div>
-// 	<div><input type="submit" value="Save"></div>
-// 	</form>
-// }
+func html(){
+	<h1>editing {{title}}</h1>
+	<form action="/save/{{.Title}}" method="POST">
+	<div><textarea name="body" rows="20" cols="80">{{printf "%s" .Body}}</textarea></div>
+	<div><input type="submit" value="Save"></div>
+	</form>
+}
 
-// func editHandler(w http.ResponseWriter, r *http.Request) {
-//     title := r.URL.Path[len("/edit/"):]
-//     p, err := loadPage(title)
-//     if err != nil {
-//         p = &Page{Title: title}
-//     }
-//     t, _ := template.ParseFiles("edit.html")
-//     t.Execute(w, p)
-// }
+func editHandler(w http.ResponseWriter, r *http.Request) {
+    title := r.URL.Path[len("/edit/"):]
+    p, err := loadPage(title)
+    if err != nil {
+        p = &Page{Title: title}
+    }
+    t, _ := template.ParseFiles("edit.html")
+    t.Execute(w, p)
+}
+
+
+//trying golang intregation
