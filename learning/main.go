c9642be17346cc7eb244bb0263f64aca4daeb7c7
Samir Mishra <smir.mishra1551@gmail.com>
2021-05-30 23:14:02 +0545

    learning golang

---
+++ learning/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/view/", ViewHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func ViewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadpage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
--- test.go
+++ test.go
@@ -11,7 +11,97 @@ func main() {
 	x = 1
 	y = 2
 
+	fmt.Println(x)
+	fmt.Println(y)
 	fmt.Println(x - y)
 	//declaring variable in different ways!
 
+}
+
+func Second() {
+	var x string = "first name"
+	var y string = "last name"
+
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(x + y)
+}
+
+func Third() {
+	x := "first name"
+	y := "last name"
+
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(x + y)
+}
+
+//trying integer variable
+
+func FirstNumber() {
+	var a = 12
+	var b = 13
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(a + b)
+
+}
+
+func SecondNumber() {
+	var a int = 12
+	var b int = 13
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(a + b)
+}
+
+func ThirdNumber() {
+	a := 12
+	b := 13
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(a + b)
+}
+
+//trying constant
+
+func FirstConstant() {
+	const a string = "first name"
+	const b string = "last name"
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(a + b)
+}
+
+func SecondConstant() {
+	const a = "first name"
+	const b = "last name"
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(a + b)
+}
+
+//trying integer constant
+
+func FirstConstantInteger() {
+	const a int = 12
+	const b int = 13
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(a + b)
+}
+
+func SecondConstantInteger() {
+	const a = "first name"
+	const b = "last name"
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(a + b)
 }
