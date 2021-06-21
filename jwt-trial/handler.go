f796fde9a9fdd7a04e6ab707596cc8aefc868378
Samir Mishra <smir.mishra1551@gmail.com>
2021-06-21 23:01:24 +0545

    adding jwt-api-trial

---
+++ jwt-trial/handler.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+)
+
+var jwtkey = []byte("secret_key")
+
+var users = map[string]string{
+	"user1": "password1",
+	"user2": "password2",
+}
+
+type Credentials struct {
+	Username string `json:"username"`
+	password string `json:"password"`
+}
+
+type Claims struct {
+	Username string `json:"username"`
+	jwt.StandardClaims
+}
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	var credentials Credentials
+	err := json.NewDecoder(r.Body).Decode(&credentials)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	expectedPassword, ok := users[credentials.Username]
+	if !ok || expectedPassword != credentials.password {
+		w.WriteHeader(http.StatusUnauthorized)
+	}
+
+	expirationTime := time.Now().Add(time.Minute * 5)
+
+	claims := &Claims{
+		Username: credentials.Username,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtkey)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	http.SetCookie(w, &http.Cookie{
+		Name:    "token",
+		Value:   tokenString,
+		Expires: expirationTime,
+	})
+}
+
+func Home(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func Refresh(w http.ResponseWriter, r *http.Request) {
+
+}
