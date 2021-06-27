87f9b8497904de411d7ea59d2942fbb6c6944368
Samir Mishra <smir.mishra1551@gmail.com>
2021-06-27 22:31:09 +0545

    learning

--- main.go
+++ main.go
@@ -15,23 +15,7 @@ func main() {
 	http.ListenAndServe(":8080", nil)
 }
 
-func html(){
-	<h1>editing {{title}}</h1>
-	<form action="/save/{{.Title}}" method="POST">
-	<div><textarea name="body" rows="20" cols="80">{{printf "%s" .Body}}</textarea></div>
-	<div><input type="submit" value="Save"></div>
-	</form>
-}
 
-func editHandler(w http.ResponseWriter, r *http.Request) {
-    title := r.URL.Path[len("/edit/"):]
-    p, err := loadPage(title)
-    if err != nil {
-        p = &Page{Title: title}
-    }
-    t, _ := template.ParseFiles("edit.html")
-    t.Execute(w, p)
-}
 
 
 //trying golang intregation
